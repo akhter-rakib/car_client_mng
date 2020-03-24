0ac021b030d0146cda036897d3f7d944b3b46abf
akter_rakib <rakibccj@gmail.com>
2020-03-25 00:35:16 +0600

    project initiate

---
+++ src/test/java/com/vhaibrothers/car_client/CarClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhaibrothers.car_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
